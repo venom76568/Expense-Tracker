7b7a7629f5d744bf49dc6055e90d082e2ff1ae09
Harsh agrawal <139165985+venom76568@users.noreply.github.com>
2024-07-24 00:22:10 +0530

    Add files via upload

---
+++ src/components/Expenses.js
@@ -0,0 +1,33 @@
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import Card from "./card";
+import "./Expenses.css";
+import ExpensesFilter from "./ExpensesFilter";
+
+import React, { useState } from "react";
+function Expenses(props) {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+}
+
+export default Expenses;
